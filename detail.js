cab3d0b824554bafaaae81653944eedc862af5d5
Nadia Lutz <na.lutz@outlook.com>
2025-03-05 12:31:37 +0100

    layout for both pages nearly complete, added a lot of styling and optimised a lot of HTML structure and removed duplicate and unnecessary content

--- detail.js
+++ detail.js
@@ -9,6 +9,21 @@ let options = {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
+  let headerDetails = document.createElement("header")
+  headerDetails.classList.add("header__details");
+  headerDetails.innerHTML = `
+    <button class="return__btn">
+      <a href="index.html">
+        <img src="img/back.png">
+      </a>
+    </button>
+    <label class="switch">
+      <input type="checkbox" name="switch" id="switch">
+      <span class="slider round"></span>
+    </label>
+  `;
+
+  document.body.insertBefore(headerDetails, document.body.firstChild);
 
   let urlParams = new URLSearchParams(window.location.search);
   let movieId = urlParams.get('id'); 
@@ -16,50 +31,54 @@ document.addEventListener('DOMContentLoaded', () => {
   if (movieId) {
     fetchMovieDetails(movieId);
   }
-});
 
-function fetchMovieDetails(movieId) {
-  let movieDetailsUrl = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&api_key=${apiKey}`;
-  let movieCreditsUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`;
+  function fetchMovieDetails(movieId) {
+    let movieDetailsUrl = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&api_key=${apiKey}`;
+    let movieCreditsUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`;
 
+    Promise.all([fetch(movieDetailsUrl, options), fetch(movieCreditsUrl, options)])
+      .then(([movieRes, creditsRes]) => Promise.all([movieRes.json(), creditsRes.json()]))
+      .then(([movie, credits]) => {
+        let detailSection = document.querySelector('.detail__section');
+        if (!detailSection) return;
 
-  Promise.all([fetch(movieDetailsUrl, options), fetch(movieCreditsUrl, options)])
-    .then(([movieRes, creditsRes]) => Promise.all([movieRes.json(), creditsRes.json()]))
-    .then(([movie, credits]) => {
-      let detailSection = document.querySelector('.detail__section');
-      if (!detailSection) return;
-
-  
-      detailSection.innerHTML = `
-        <div class="movie__details">
-          <img src="${baseUrl}${movie.poster_path}" alt="${movie.title}" loading="lazy">
-          <h2>${movie.title}</h2>
-          <div>
-               <img src="img/star.png" class="star__img"> 
-               <p>${movie.vote_average}/10</p>
+        detailSection.innerHTML = `
+          <div class="movie__details">
+            <div class="movie__details-top">
+              <h1>${movie.title}</h1>
+              <img src="img/bookmarksave.png">
+            </div>
+            <div class="rating__details details">
+              <img src="img/star.png" class="star__img"> 
+              <p>${movie.vote_average}/10 IMDb</p>
+            </div>
+            <ul class="movie__genres">
+              ${movie.genres.map((genre) => `<li class="genre__items">${genre.name}</li>`).join('')}
+            </ul>
+            <div class="info__item-container">
+              <li class="info__item"><span>Length</span> <p>${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}min</p></li>
+              <li class="info__item"><span>Language</span> <p>${movie.original_language}</p></li>
+              <li class="info__item"><span>Rating</span> <p>${movie.rating}</p></li>
+            </div>
+            <h2>Description</h2>
+            <p>${movie.overview}</p>
           </div>
-          <ul class="movie__genres">
-          <li class="genre__items">${movie.genres.map(genre => genre.name).join(', ')}</li>
-          </ul>
-          <p>Description: ${movie.overview}</p>
-          <p class="info__item">Length: ${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60} min</p>
-          <p class="info__item">Language: ${movie.original_language}</p>
-        </div>
 
-        <section class="cast__section">
+          <section class="cast__section">
             <div class="cast__section-top">
-                <h2>Cast</h2>
-                <button id="seemoreBtn" class"seemore__btn>See more</button>
+              <h2>Cast</h2>
+              <button id="seemoreBtn" class="seemore__btn">See more</button>
             </div>
             <ul class="cast__item-container">
-                ${credits.cast && credits.cast.length ? credits.cast.map(actor => {
-                    return `<li class="cast__item">
-                        <img src="${baseUrl}${actor.profile_path}" alt="${actor.name}" loading="lazy">
-                        <p>${actor.name}</p>
-                    </li>`;
-                }).join('') : ''}
+              ${credits.cast && credits.cast.length ? credits.cast.map(actor => {
+                return `<li class="cast__item">
+                          <img src="${baseUrl}${actor.profile_path}" alt="${actor.name}" loading="lazy">
+                          <p>${actor.name}</p>
+                        </li>`;
+              }).join('') : ''}
             </ul>
-        </section>
-      `;
-    })
-}
+          </section>
+        `;
+      });
+  }
+});
--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-let apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTZjNWNkNjcwYWJlZDUzMDVmMDgyOTg2ZTJlMjQ4ZSIsIm5iZiI6MTc0MDk4Njc5MS4xODMsInN1YiI6IjY3YzU1OWE3ODgxYzAxM2VkZTdhNmZiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5GC0q7C8tlc2UOU8SLns_FNXlBD6J1ZwLRs4DDdcFhU';  // Use your actual API key here (replace 'your_api_key' with the valid API key)
+let accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTZjNWNkNjcwYWJlZDUzMDVmMDgyOTg2ZTJlMjQ4ZSIsIm5iZiI6MTc0MDk4Njc5MS4xODMsInN1YiI6IjY3YzU1OWE3ODgxYzAxM2VkZTdhNmZiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5GC0q7C8tlc2UOU8SLns_FNXlBD6J1ZwLRs4DDdcFhU';
 let baseUrl = 'https://image.tmdb.org/t/p/w500';
-let options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${apiKey}`
-  }
-};
-
 let genreMapping = {
   12: 'Adventure', 14: 'Fantasy', 16: 'Animation', 18: 'Drama', 27: 'Horror',
   28: 'Action', 35: 'Comedy', 36: 'History', 37: 'Western', 53: 'Thriller',
@@ -16,7 +8,7 @@ let genreMapping = {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  let headerElm = document.createElement("header");
+  let headerElm = document.createElement('header');
   headerElm.innerHTML = `
     <h1>MyMovies</h1>
     <label class="switch">
@@ -28,40 +20,53 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let currentOffset = 1;
 
-  let mainElm = document.createElement("main");
-  let movieListContainer = document.createElement("section");
+  let mainElm = document.createElement('main');
+  let movieListContainer = document.createElement('section');
   movieListContainer.innerHTML = `
     <div class="movielist__top">
       <h2>Now Showing</h2>
       <button id="seeMore" class="seemore__btn">See more</button>
     </div>
   `;
-  let movieList = document.createElement("ul");
-  movieList.classList.add("movielist");
+  let movieList = document.createElement('ul');
+  movieList.classList.add('movielist');
   movieListContainer.appendChild(movieList);
 
-  let popularListContainer = document.createElement("section");
+  let popularListContainer = document.createElement('section');
   popularListContainer.innerHTML = `
     <div class="popularlist__top">
       <h2>Popular</h2>
       <button id="seemoreBtn" class="seemore__btn">See more</button>
     </div>
   `;
-  let popularList = document.createElement("ul");
-  popularList.classList.add("popularlist");
+  let popularList = document.createElement('ul');
+  popularList.classList.add('popularlist');
   popularListContainer.appendChild(popularList);
 
   mainElm.appendChild(movieListContainer);
   mainElm.appendChild(popularListContainer);
   document.body.appendChild(mainElm);
 
-  function fetchMovielist(page) {
-    let url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}&api_key=${apiKey}`;
-    fetch(url)
-      .then(res => res.json())
-      .then(json => {
+  let fetchMovielist = (page) => {
+    let url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}&api_key=${accessToken}`;
+    let options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    fetch(url, options)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((json) => {
         if (json.results && json.results.length > 0) {
-          json.results.forEach(movie => {
+          let movieList = document.querySelector('.movielist');
+          json.results.forEach((movie) => {
             let movieListItem = document.createElement('li');
             movieListItem.className = 'movielist__item';
             let posterPath = movie.poster_path;
@@ -71,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <img src="${imageUrl}" alt="${movie.title}" loading="lazy">
               </a>
               <h3>${movie.title}</h3>
-              <div>
+              <div class="rating__details">
                 <img src="img/star.png" class="star__img"> 
                 <p>${movie.vote_average.toFixed(1)}/10 IMDb</p>
               </div>
@@ -79,36 +84,41 @@ document.addEventListener('DOMContentLoaded', () => {
             movieList.appendChild(movieListItem);
           });
         }
-      });
+      })
+      .catch((error) => {});
+  };
 
-    let footer = document.createElement("footer");
-    footer.innerHTML = `
-      <ul>
-        <li><a href="index.html"><img src="img/bookmark.png"></a></li>
-        <li><a href="#"><img src="img/bookmarkcopy.png"></a></li>
-        <li><a href="#"><img src="img/bookmarksave.png"></a></li>
-      </ul>
-    `;
-    document.body.appendChild(footer);
-  }
-
-  function fetchPopularlist() {
-    let url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${apiKey}`;
-    fetch(url)
-      .then(res => res.json())
-      .then(json => {
+  let fetchPopularlist = () => {
+    let url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${accessToken}`;
+    let options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    fetch(url, options)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((json) => {
         if (json.results && json.results.length > 0) {
-          json.results.forEach(movie => {
-            fetch(`https://api.themoviedb.org/3/movie/${movie.id}?language=en-US&api_key=${apiKey}`)
-              .then(res => res.json())
-              .then(movieDetails => {
+          let popularList = document.querySelector('.popularlist');
+          json.results.forEach((movie) => {
+            fetch(`https://api.themoviedb.org/3/movie/${movie.id}?language=en-US&api_key=${accessToken}`, options)
+              .then((res) => res.json())
+              .then((movieDetails) => {
                 let popularItem = document.createElement('li');
                 popularItem.className = 'popular__item';
                 let posterPath = movie.poster_path;
                 let imageUrl = posterPath ? `${baseUrl}${posterPath}` : '';
-                let genres = movie.genre_ids.map(genreId => genreMapping[genreId] || 'Unknown Genre');
-
-                let runtime = movieDetails.runtime ? `${Math.floor(movieDetails.runtime / 60)}h ${movieDetails.runtime % 60} minutes` : 'Runtime not available';
+                let genres = movie.genre_ids.map((genreId) => genreMapping[genreId] || 'Unknown Genre');
+                let runtime = movieDetails.runtime
+                  ? `${Math.floor(movieDetails.runtime / 60)}h ${movieDetails.runtime % 60} minutes`
+                  : 'Runtime not available';
 
                 popularItem.innerHTML = `
                   <div class="popular__item-left">
@@ -118,33 +128,33 @@ document.addEventListener('DOMContentLoaded', () => {
                   </div>
                   <div class="popular__item-right">
                     <h3>${movie.title}</h3>
-                    <div>
+                    <div class="rating__details">
                       <img src="img/star.png" class="star__img"> 
                       <p>${movie.vote_average.toFixed(1)}/10 IMDb</p>
                     </div>
                     <ul class="movie__genres">
-                      ${genres.map(genre => `<li class="genre__items">${genre}</li>`).join('')}
+                      ${genres.map((genre) => `<li class="genre__items">${genre}</li>`).join('')}
                     </ul>
-                    <span><img src="img/clock.png">${runtime}</span>
+                    <span><img src="img/clock.png"><p>${runtime}</p></span>
                   </div>
                 `;
                 popularList.appendChild(popularItem);
-              })
+              });
           });
         }
-      });
-  }
+      })
+      .catch((error) => {});
+  };
 
-  let observer = new IntersectionObserver(function(entries) {
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting) {
-        currentOffset += 1;
-        fetchMovielist(currentOffset);
-      }
-    });
-  }, {
-    rootMargin: '200px',
-  });
+  let footerElm = document.createElement('footer');
+  footerElm.innerHTML = `
+  <ul>
+    <li><img src="img/bookmark.png"></li>
+    <li><img src="img/bookmarkcopy.png"></li>
+    <li><img src="img/bookmarksave.png"></li>
+</ul>`
+
+document.body.appendChild(footerElm);
 
   fetchMovielist(currentOffset);
   fetchPopularlist();
