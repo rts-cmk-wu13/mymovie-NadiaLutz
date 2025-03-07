c34c8a968cad07d53ba87bbfa3b62c599877fd16
Nadia Lutz <na.lutz@outlook.com>
2025-03-07 13:57:55 +0100

    added headlines for the new pages and added style.display= 'block' and 'none' etc

--- layout.js
+++ layout.js
@@ -7,31 +7,52 @@ document.addEventListener('DOMContentLoaded', () => {
       <a href="index.html">
         <img src="img/back.png" alt="Back">
       </a>
-      <button class="return__btn-more">
+    </button>
+    <button class="return__btn-more">
       <a href="index.html">
-        <img src="img/back_grey.png" alt="Back">
+        <img src="img/back_grey.png" alt="Back" class="back__img-grey">
       </a>
     </button>
+    <h1 class="headline__now">Now Showing</h1>
+    <h1 class="headline__popular">Popular</h1>
     <label class="switch">
       <input type="checkbox" name="switch" id="switch">
       <span class="slider round"></span>
     </label>
   `;
-  
+
   document.body.insertBefore(headerElm, document.body.firstChild);
 
   let currentPage = document.location.pathname;
 
+  if (currentPage.includes('index.html')) {
+    document.querySelector('.return__btn').style.display = 'none';
+    document.querySelector('.return__btn-more').style.display = 'none';
+    document.querySelector('.headline__now').style.display = 'none';
+    document.querySelector('.headline__popular').style.display = 'none';
+  }
 
   if (currentPage.includes('detail.html')) {
     headerElm.querySelector('.return__btn').style.display = 'block';
     headerElm.querySelector('.return__btn-more').style.display = 'none';
     headerElm.querySelector('.index__headline').style.display = 'none';
-  } 
+    headerElm.querySelector('.headline__now').style.display = 'none';
+    headerElm.querySelector('.headline__popular').style.display = 'none';
+  }
 
   if (currentPage.includes('showmore_now.html') || currentPage.includes('showmore_popular.html')) {
     headerElm.querySelector('.return__btn-more').style.display = 'block';
     headerElm.querySelector('.return__btn').style.display = 'none';
     headerElm.querySelector('.index__headline').style.display = 'none';
+  }
+
+  if (currentPage.includes('showmore_now.html')) {
+    headerElm.querySelector('.headline__now').style.display = 'block';
+    headerElm.querySelector('.headline__popular').style.display = 'none';
+  }
+
+  if (currentPage.includes('showmore_popular.html')) {
+    headerElm.querySelector('.headline__now').style.display = 'none';
+    headerElm.querySelector('.headline__popular').style.display = 'block';
   }
 });
